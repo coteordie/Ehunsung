e7df8d90eea5dacc6462df1e255d25e95801cb48
jess0402 <jyjmjs2@naver.com>
2022-09-16 22:40:33 +0900

    feat: 2022/09/16 문제풀이

---
+++ Study1-BruteForce/Week01/Day01/BOJ_2798.java
@@ -0,0 +1,39 @@
+package bruteForce;
+
+import java.util.Scanner;
+
+public class BOJ_2798 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+	
+		int[] cards = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			cards[i] = sc.nextInt();
+		}
+		
+		int sum = 0;
+		int answer = 0;
+		
+		for(int i = 0; i < cards.length-2; i++) {
+			for(int j = i + 1; j < cards.length-1; j++) {
+				for(int k = j + 1; k < cards.length; k++) {
+					
+					sum = cards[i] + cards[j] +  cards[k];
+					
+					if(sum == m) {
+						System.out.println(sum);
+						return;
+					}
+					else if(sum < m && sum > answer)
+						answer = sum;
+				}
+			}
+		}
+		
+		System.out.print(answer);
+	}
+}
