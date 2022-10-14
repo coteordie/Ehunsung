43fa31e5c4e8fc63acf579a1837d4069076b8e33
jess0402 <jyjmjs2@naver.com>
2022-10-15 00:25:20 +0900

    feat: 2022/10/13 문제풀이

---
+++ Study4-SolvedClass1++/Week5/Day20/BOJ_1008.java
@@ -0,0 +1,20 @@
+package solvedClass1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1008 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		double a = Double.parseDouble(st.nextToken());
+		double b = Double.parseDouble(st.nextToken());
+		
+		System.out.print(a/b);
+	}
+
+}
