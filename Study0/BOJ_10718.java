61d484a9e8f3957dac7a00b7068a185d200dd51b
jess0402 <jyjmjs2@naver.com>
2022-10-16 20:21:44 +0900

    feat: 2022/10/14 문제풀이

---
+++ Study0/BOJ_10718.java
@@ -0,0 +1,17 @@
+package my;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class BOJ_10718 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("강한친구 대한육군");
+		bw.newLine();
+		bw.write("강한친구 대한육군");
+		
+		bw.close();
+	}
+
+}
---
+++ Study0/BOJ_1978.java
@@ -0,0 +1,39 @@
+package my;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_1978 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");		
+		int cnt = 0;
+		int total = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int j = Integer.parseInt(st.nextToken());
+			for(int k = 2; k <= j; k++) {
+				if(j % k == 0) {
+					cnt++;
+				}
+			}
+			
+			if(cnt == 1) {
+				total++;
+			}
+			cnt = 0;			
+		}
+		
+		bw.write(String.valueOf(total));
+		bw.close();
+	}
+
+}
