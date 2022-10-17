3c6b93cb1e56488eb69cc17c73be15f3cca48829
jess0402 <jyjmjs2@naver.com>
2022-10-17 19:52:25 +0900

    feat: 2022/10/17 문제풀이

---
+++ Study0/BOJ_10926.java
@@ -0,0 +1,24 @@
+package my;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_10926 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(br.readLine());
+		sb.append("??!");
+		
+		bw.write(String.valueOf(sb));
+		bw.close();
+	
+	}
+
+}
